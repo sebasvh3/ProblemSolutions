08a1c00e5110634e48f96b061ffa96c8b6e9deeb
jvahos <sebas326@gmail.com>
2014-07-23 17:18:01 -0500

    Probles 276A, 431A y 448A

---
+++ CodeForces/276A.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define IO ios_base::sync_with_stdio(false);cin.tie(NULL)
+
+#define endl "\n"
+#define D(x) cout << #x << " : " << x << "  "
+#define l cout << endl
+#define in freopen("in","r",stdin)
+
+#define For(i,a)    for(int i=0; i<a; i++)
+#define Fori(i,a,b) for(int i=a; i<b; i++)
+#define pb push_back
+#define mp make_pair
+#define all(x)  x.begin(),x.end()
+#define allr(x) x.rbegin(),x.rend()
+
+typedef vector<int> vi;
+typedef pair<int,int> pii;
+typedef vector<pii > vpii;
+typedef long long int llint;
+
+
+int main() { IO;
+    int n,k,f,t;
+    cin>>n>>k;
+    int may=INT_MIN;
+    int tem;
+    For(i,n) {
+        cin>>f>>t;
+        tem = t>k ? f-t+k : f;
+        if(tem>may) may=tem;
+    }
+    
+    cout<<may<<endl;
+    
+}
---
+++ CodeForces/431A.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define IO ios_base::sync_with_stdio(false);cin.tie(NULL)
+
+#define endl "\n"
+#define D(x) cout << #x << " : " << x << "  "
+#define l cout << endl
+#define in freopen("in","r",stdin)
+
+#define For(i,a)    for(int i=0; i<a; i++)
+#define Fori(i,a,b) for(int i=a; i<b; i++)
+#define pb push_back
+#define mp make_pair
+#define all(x)  x.begin(),x.end()
+#define allr(x) x.rbegin(),x.rend()
+
+typedef vector<int> vi;
+typedef pair<int,int> pii;
+typedef vector<pii > vpii;
+typedef long long int llint;
+
+
+int main() { IO;
+    int x;
+    string nums;
+    int cal[5]={0};
+    Fori(i,1,5) {
+        cin>>x;
+        cal[i]=x;
+    }
+    cin >> nums;
+    int cant=0;
+    For(i,nums.size()) {
+        cant+=cal[nums[i]-48];
+    }
+    cout << cant;
+}
