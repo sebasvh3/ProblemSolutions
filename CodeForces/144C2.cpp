eef829a56b964db8080edcfb6869729163edba96
jvahos <sebas326@gmail.com>
2014-09-12 15:27:02 -0500

    Full Solition 'Anagram Search'

--- CodeForces/144C2.cpp
+++ CodeForces/144C2.cpp
@@ -23,17 +23,15 @@ int main() { IO;
     int n;
     string s,p;
     cin>>s>>p;
-    int ssub = 1;
     int ss = s.size(), sp = p.size();
-    
     bool first = true;
-    
     if(ss<sp) cout<<"0"<<endl;
     else {
+        int ssub = 1;
         int comodin=0, times=0;
         int a[26]={0};
         int b[26]={0};
-        For(i,26) a[p[i]-'a']++;
+        For(i,sp) a[p[i]-'a']++;
         For(i,ss) {
             if(s[i]=='?') comodin++;
             else b[s[i]-'a']++;
@@ -43,14 +41,8 @@ int main() { IO;
                     if(b[j]<a[j]) dif+= a[j]-b[j];
                 }
                 if(dif==0 or comodin>=dif) times++;
-                //--
-                //if(first) first=false;
-                //else{
-                    if(s[i-sp+1]=='?') comodin--;
-                    else b[s[i-sp+1]-'a']--;
-                D(s[i-sp+1]);l;
-                D(b[s[i-sp+1]-'a']);l;
-                //}
+                if(s[i-sp+1]=='?') comodin--;
+                else b[s[i-sp+1]-'a']--;
             }
             ssub++;
         }
