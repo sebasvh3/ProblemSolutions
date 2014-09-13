ee7ac5ace408d58360b5dac96f2175b9b0d961bb
sebasvh <sebas326@gmail.com>
2014-09-13 18:27:52 -0500

    Problem 203B continue

--- CodeForces/203B.cpp
+++ CodeForces/203B.cpp
@@ -21,7 +21,37 @@ typedef long long int llint;
 
 
 int main() { IO;
-    int n;
-    cin>>n;
+    int n,m;
+    cin>>n>>m;
     vector<vi> nums(n,vi(n,0));
+    int x,y,times=0;
+    For(i,m) {
+        cin>>x>>y;
+        nums[x-1][y-1]=1;//index 0
+        times++;
+        
+    }
+    
+    for(int i=1; i<n-1; i++) {
+        for(int j=1; j<n-1; j++) {
+            if(nums[i][j]==1) {
+                bool sol = true;
+                For(k,9) {
+                    if(nums[i+dx][j+dy] != 1){
+                        sol=false;
+                        break;
+                    }
+                    if(sol){
+                    }
+                }
+            }
+        }
+    }
+    
+    For(i,n) {
+        For(j,n) {
+            cout<<nums[i][j]<<" ";
+        }l;
+    }
+    return 0;
 }
