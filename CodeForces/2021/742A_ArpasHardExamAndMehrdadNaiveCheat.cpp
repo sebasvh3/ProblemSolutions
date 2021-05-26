dc11dd997119194983e150896de5ea158a633634
Sebastian Vahos <sebas326@gmail.com>
2021-05-26 16:19:09 -0500

    Create 742A_ArpasHardExamAndMehrdadNaiveCheat.cpp

---
+++ CodeForces/2021/742A_ArpasHardExamAndMehrdadNaiveCheat.cpp
@@ -0,0 +1,14 @@
+/*
+ * Name : 742A - Arpa’s hard exam and Mehrdad’s naive cheat
+ * Url  : https://codeforces.com/problemset/problem/742/A
+ * Sub  : https://codeforces.com/contest/742/submission/117445331
+ */
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n, lastDigit[] = {6,8,4,2};
+    cin >> n;
+    cout << (n==0 ? 1 : lastDigig[n%4]);
+}
